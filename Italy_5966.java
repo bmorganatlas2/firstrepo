ac97f8a89a0fe0c92934d6f623fda5fdc4d9a4d1
Finn Senger DVM <dvm.finn.senger@rodriguez.org>
2015-01-07 13:42:31 +1100

    ABC-768 commit

---
+++ Italy_5966.java
@@ -0,0 +1,1 @@
+["Fugiat dolorum est et ipsum quas. Est doloremque ipsum. Possimus voluptas consequatur corrupti.", "Ratione ducimus et. Qui vel tempora voluptatem modi. Est velit aut nam. Sequi exercitationem doloremque ut. Doloremque omnis molestias at et doloribus.", "Dolore architecto dolorem in beatae eum corrupti. Sapiente doloribus ad. Et et eius quo maiores cupiditate."]
