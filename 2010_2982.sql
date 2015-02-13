0893f0e825aa4d96261badf34473fdce6ee55e82
Cindy Jewess <cindy.jewess@prohaskadaniel.com>
2015-02-14 09:22:14 +1100

    ABC-1090 commit

---
+++ 2010_2982.sql
@@ -0,0 +1,1 @@
+["Suscipit officiis et odio velit et quia animi. Inventore perferendis qui. Similique tempora placeat soluta quam nemo et recusandae. Officiis et quam neque perspiciatis. Deleniti alias ducimus cum expedita ut magni sint.", "Rem odit voluptatibus omnis assumenda. Sit et quo rem quia recusandae odio. Qui est ut deserunt omnis voluptate."]
